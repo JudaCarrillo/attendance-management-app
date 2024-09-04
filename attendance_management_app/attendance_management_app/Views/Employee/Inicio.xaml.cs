0c19543b08d47e9457791fdcce4727beae436b2c
Juda <judacarrillot@gmail.com>
2024-09-04 17:45:07 -0500

    feat: select the day to mark attendance

--- attendance_management_app/attendance_management_app/Views/Employee/Inicio.xaml.cs
+++ attendance_management_app/attendance_management_app/Views/Employee/Inicio.xaml.cs
@@ -11,33 +11,89 @@ using Xamarin.Forms.Xaml;
 
 namespace attendance_management_app.Views.Employee
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Inicio : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
         private Button _selectedButton;
-        public Inicio ()
-		{
-			InitializeComponent ();
-			LoadData();
-        }
-
-		private void LoadData()
-		{
-            User currentUser = AuthService.Instance.currentUser;
-			Turn userTurn = TurnDataStore.Instance.GetTurnsDataStore().Find(turn => turn.TurnId == currentUser.TurnId);
-			BindingContext = userTurn;
-        }
-
-        private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public Inicio()
         {
-
+            InitializeComponent();
+            LoadData();
         }
+
+        private void LoadData()
+        {
+            User currentUser = AuthService.Instance.currentUser;
+            Turn userTurn = TurnDataStore.Instance.GetTurnsDataStore().Find(turn => turn.TurnId == currentUser.TurnId);
+            BindingContext = userTurn;
+
+            //DateTime today = DateTime.Today;
+            //OnSelectDay(today);
+        }
+
+
         private void OnButtonClicked(object sender, EventArgs e)
         {
-            var clickedButton = sender as Button;
+            var button = sender as Button;
+            if (button == null) return;
 
-            if (clickedButton == null) return;
+            string clickedDay = button.CommandParameter as string;
 
+            var dayOfWeek = Util.GetDayOfWeek(clickedDay);
+            DateTime today = DateTime.Today;
+            var targetDate = Util.GetClosestDateForDayOfWeek(today, dayOfWeek);
+            OnSelectDay(targetDate);
+
+            ButtonSelectedStyle(button);
+        }
+
+        private void OnSelectDay(DateTime date)
+        {
+            string targetMonht = date.ToString("MM/yyyy");
+            string targetDate = date.ToString("dd/MM/yyyy");
+
+            var currentUser = AuthService.Instance.currentUser;
+
+            var attendanceData = AttendanceDataStore.Instance.GetAttendancesData();
+
+            if (!attendanceData.ContainsKey(targetMonht) || !attendanceData[targetMonht].ContainsKey(targetDate))
+            {
+                AttendanceDataStore.Instance.InitializedAttendanceDataStore(targetMonht, targetDate);
+            }
+
+            var record = attendanceData[targetMonht][targetDate];
+            var attendanceRecord = record.Early.Find(attendance => attendance.UserId == currentUser.UserId) ??
+                                   record.Late.Find(attendance => attendance.UserId == currentUser.UserId) ??
+                                   record.Absent.Find(attendance => attendance.UserId == currentUser.UserId);
+
+            if (attendanceRecord == null)
+            {
+                AttendanceFrame.BackgroundColor = Color.White;
+                return;
+            }
+
+            switch (attendanceRecord.Type)
+            {
+                case Attendance.AttendanceType.Early:
+                    AttendanceFrame.BackgroundColor = Color.FromHex("#801CDE3B");
+                    break;
+                case Attendance.AttendanceType.Late:
+                    AttendanceFrame.BackgroundColor = Color.FromHex("#80FFAE00");
+                    break;
+                case Attendance.AttendanceType.Absent:
+                    AttendanceFrame.BackgroundColor = Color.FromHex("#80FF0000");
+                    break;
+            }
+        }
+
+        private void OnLogoutButtonClicked(object sender, EventArgs e)
+        {
+            AuthService.Instance.Logout();
+            Navigation.PopToRootAsync();
+        }
+
+        private void ButtonSelectedStyle(Button clickedButton)
+        {
             // Restaurar el estado del botón previamente seleccionado
             if (_selectedButton != null)
             {
@@ -52,5 +108,6 @@ namespace attendance_management_app.Views.Employee
             // Guardar la referencia del botón seleccionado
             _selectedButton = clickedButton;
         }
+
     }
 }
